190bb43b4a901647e9a671c643df021a1dadfd5b
Gregory <34886930+gregorylearns@users.noreply.github.com>
2024-04-10 17:46:17 +0800

    Update python-teacher_eval_gui.py

--- python-teacher_eval_gui.py
+++ python-teacher_eval_gui.py
@@ -1,7 +1,5 @@
-import keyboard
+from pynput import keyboard
 import time
-
-
 
 print("""
 =========================================================================================
@@ -16,37 +14,55 @@ print("""
 =========================================================================================
     """)
 
-
-
 looping = False
 
 def send_keys(keys):
     print(f"Executing (0.1s delay)")
     time.sleep(1.5)
-    for key in keys:
-        keyboard.send(key)
-        time.sleep(0.1)
+    with keyboard.Controller() as controller:
+        for key in keys:
+            controller.press(key)
+            controller.release(key)
+            time.sleep(0.1)
 
 def toggle_loop():
     global looping
     looping = not looping
 
-keyboard.add_hotkey('ctrl+shift+f1', lambda: toggle_loop())
-keyboard.add_hotkey('ctrl+shift+f2', lambda: toggle_loop())
-keyboard.add_hotkey('ctrl+shift+f3', lambda: toggle_loop())
-keyboard.add_hotkey('ctrl+shift+f4', lambda: toggle_loop())
-keyboard.add_hotkey('ctrl+shift+f5', lambda: toggle_loop())
+def on_press(key):
+    try:
+        if key == keyboard.Key.ctrl_l or key == keyboard.Key.ctrl_r:
+            pass
+        elif key == keyboard.Key.shift_l or key == keyboard.Key.shift_r:
+            pass
+        elif key == keyboard.Key.f1:
+            toggle_loop()
+        elif key == keyboard.Key.f2:
+            toggle_loop()
+        elif key == keyboard.Key.f3:
+            toggle_loop()
+        elif key == keyboard.Key.f4:
+            toggle_loop()
+        elif key == keyboard.Key.f5:
+            toggle_loop()
+    except AttributeError:
+        pass
+
+listener = keyboard.Listener(on_press=on_press)
+listener.start()
 
 while True:
     if looping:
-        if keyboard.is_pressed('ctrl+shift+f1'):
-            send_keys(['tab', 'space'] * 9 + ['tab'] * 3)
-        elif keyboard.is_pressed('ctrl+shift+f2'):
-            send_keys(['tab', 'right'] * 9 + ['tab'] * 3)
-        elif keyboard.is_pressed('ctrl+shift+f3'):
-            send_keys(['tab', 'right', 'right'] * 9  + ['tab'] * 3)
-        elif keyboard.is_pressed('ctrl+shift+f4'):
-            send_keys(['tab', 'right', 'right', 'right'] * 9 + ['tab'] * 3)
-        elif keyboard.is_pressed('ctrl+shift+f5'):
-            send_keys(['tab', 'right', 'right', 'right', 'right'] * 9 + ['tab'] * 3)
+        if keyboard.Key.ctrl_l in listener.pressed_keys or keyboard.Key.ctrl_r in listener.pressed_keys:
+            if keyboard.Key.shift_l in listener.pressed_keys or keyboard.Key.shift_r in listener.pressed_keys:
+                if keyboard.Key.f1 in listener.pressed_keys:
+                    send_keys(['tab', 'space'] * 9 + ['tab'] * 3)
+                elif keyboard.Key.f2 in listener.pressed_keys:
+                    send_keys(['tab', 'right'] * 9 + ['tab'] * 3)
+                elif keyboard.Key.f3 in listener.pressed_keys:
+                    send_keys(['tab', 'right', 'right'] * 9 + ['tab'] * 3)
+                elif keyboard.Key.f4 in listener.pressed_keys:
+                    send_keys(['tab', 'right', 'right', 'right'] * 9 + ['tab'] * 3)
+                elif keyboard.Key.f5 in listener.pressed_keys:
+                    send_keys(['tab', 'right', 'right', 'right', 'right'] * 9 + ['tab'] * 3)
     time.sleep(0.1)
